9b458f757a7363d28c5690700495eeb91c5edf10
Ayberk Akici <ayberk.akici@thetimemaster.de>
2017-11-24 16:42:00 +0100

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
@@ -33,6 +34,26 @@ app.get('/todos', (req, res)=>{
         res.status(400).send(e);
     });
 
+});
+
+// GET /todos/1234
+app.get('/todos/:id', (req, res)=>{
+
+    const id = req.params.id;
+
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo)=>{
+        if(!todo) {
+            return res.status(404).send();
+        }
+
+        res.status(200).send({todo});
+    }).catch(e => {
+        res.status(400).send();
+    });
 });
 
 app.listen(3000, ()=>{
